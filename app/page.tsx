0f9a5fc912195ca55cc845d7e36f5b0f0658fd7b
mamoud <mamoud.antacid895@passmail.com>
2024-12-16 21:34:34 +0100

    feat: responsive presence section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 
 import { Hero } from "@/components/sections/hero";
+import { Presence } from "@/components/sections/presence";
+
+
 export default function Home() {
   return (
     <section className="container mx-auto">
       <Hero />
+      <Presence />
     </section>
   );
 }
